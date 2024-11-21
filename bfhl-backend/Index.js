75050918cacc4d2a45ab35e7ed938d7f269b7dca
Krish Ojha <11krishojha08@gmail.com>
2024-11-21 18:12:00 +0530

    first commit

--- bfhl-backend/Index.js
+++ bfhl-backend/Index.js
@@ -73,7 +73,8 @@ function isPrime(num) {
 }
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || "https://bajaj-navy-delta.vercel.app/";
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
